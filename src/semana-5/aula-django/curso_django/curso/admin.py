93e89294fae8ba5171afe7a687323bc11b5db0a0
gabriel-lumertz <glumertz10@gmail.com>
2024-10-01 23:13:13 -0300

    Semana 8 aula 2

--- src/semana-5/aula-django/curso_django/curso/admin.py
+++ src/semana-5/aula-django/curso_django/curso/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Turma
+from .models import Turma, AtividadeAluno, Aluno, Professor
 
 # Register your models here.
+
+
+def marcar_como_inativa(modeladmin, request, queryset):
+    queryset.update(ativa=False)
+    modeladmin.message_user(request, 'Turmas marcadas como inativas.')
 
 
 class TurmaAdmin(admin.ModelAdmin):
@@ -15,32 +20,62 @@ class TurmaAdmin(admin.ModelAdmin):
     )
     list_filter = ('ativa',)
     fieldsets = (
-        (
-            'Informações da turma', {
+        ('Informações da turma', {
                 'fields': ('nome', 'ativa')
-            }
-        ),
-        (
-            'Datas', {
-                'fields': (
-                    'inicio_aulas',
-                    'fim_aulas'
-                )
-            }
-        ),
-        (
-            'Participantes', {
-                'fields': (
-                    'professores',
-                    'alunos'
-                )
-            }
-        )
+        }),
+        ('Datas', {
+            'fields': ('inicio_aulas', 'fim_aulas')
+        }),
+        ('Participantes', {
+            'fields': ('professores', 'alunos')
+        })
     )
     filter_horizontal = (
         'professores',
         'alunos'
     )
+    readonly_fields = ('nome',)
+
+    actions = [marcar_como_inativa]
 
 
+class AtividadeAlunoInline(admin.StackedInline):
+    model = AtividadeAluno
+    fields = ('atividade', 'data_entrega', 'nota')
+    readonly_fields = ('nota',)
+
+
+class AtividadeAlunoTabularInline(admin.TabularInline):
+    model = AtividadeAluno
+    fields = ('atividade', 'data_entrega', 'nota')
+
+
+class AlunoAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Informações pessoais', {
+            'fields': ('nome', 'data_nascimento', 'mentor')
+        }),
+        ('Informações extras', {
+            'fields': ('data_matricula',)
+        })
+    )
+    list_display = ('nome', 'data_nascimento', 'mentor')
+
+    inlines = [AtividadeAlunoTabularInline]
+
+
+class ProfessorAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Informações pessoais', {
+            'fields': ('nome', 'data_nascimento')
+        }),
+        ('Informações extras', {
+            'fields': ('data_contratacao', 'ativo')
+        })
+    )
+    list_display = ('nome', 'data_nascimento', 'ativo')
+
+
+admin.site.register(Professor, ProfessorAdmin)
+admin.site.register(Aluno, AlunoAdmin)
 admin.site.register(Turma, TurmaAdmin)
